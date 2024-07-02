91767e4a24bccf6ddd6d42b71c16237000df052a
César Mayta <cesarmayta@gmail.com>
2024-07-01 20:41:04 -0500

    monedas con while

--- dia1/03-bucles.py
+++ dia1/03-bucles.py
@@ -3,3 +3,7 @@ tabla = int(input("INGRESE LA TABLA A MULTIPLICAR : "))
 for contador in range(1,13,1):
     print(f"{tabla} x {contador} = {tabla * contador}")
 
+contador = 1
+while(contador <= 12):
+    print(f"{tabla} x {contador} = {tabla * contador}")
+    contador += 1
