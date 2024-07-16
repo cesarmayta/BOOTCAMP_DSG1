fa02b959d91f186fe886f4197e2c43b81093e0da
César Mayta <cesarmayta@gmail.com>
2024-07-15 19:55:32 -0500

    ejemplo listo

--- dia1/01-python-mysql.py
+++ dia1/01-python-mysql.py
@@ -4,4 +4,18 @@ connection = mysql.connector.connect(user='root',password='root',host='localhost
 
 print("estas conectado a la base de datos")
 
+alumnos_cursor = connection.cursor()
+
+#alumnos_cursor.execute("insert into tbl_alumno(nombre,email,celular) values('Ana','ana@gmail.com','77777')")
+#connection.commit()
+#print("alumno insertado")
+
+alumnos_cursor.execute("select * from tbl_alumno")
+resultado = alumnos_cursor.fetchall()
+print(resultado)
+for registro in resultado:
+    print('*'*50)
+    print(f'NOMBRE : {registro[1]}')
+    print(f'EMAIL : {registro[2]}')
+
 connection.close()
