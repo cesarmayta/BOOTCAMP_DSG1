b76afe1769d1fabb39509799827d8efec0355dda
César Mayta <cesarmayta@gmail.com>
2024-07-16 20:27:44 -0500

    herencia

--- repaso/cliente.py
+++ repaso/cliente.py
@@ -1,4 +1,12 @@
 from empresa import Empresa
 
 class Cliente(Empresa):
-    pass
+    
+    def __init__(self,ruc,rs,dir,cred):
+        super().__init__(ruc,rs,dir)
+        self.linea_credito = cred
+    
+    def mostrar(self):
+        print('*'*50 + "DATOS DEL CLIENTE")
+        super().mostrar()
+        print(f'LINEA DE CREDITO : {self.linea_credito}')
--- repaso/proveedor.py
+++ repaso/proveedor.py
@@ -1,4 +1,11 @@
 from empresa import Empresa
 
 class Proveedor(Empresa):
-    pass
+    def __init__(self,ruc,rs,dir,cont):
+        super().__init__(ruc,rs,dir)
+        self.contacto = cont
+    
+    def mostrar(self):
+        print('*'*50 + "DATOS DEL PROVEEDOR")
+        super().mostrar()
+        print(f'PERSONA DE CONTACTO : {self.contacto}')
