92b22ffd1e232a947477e55c058aa23b65dcfcf4
César Mayta <cesarmayta@gmail.com>
2024-07-19 20:56:27 -0500

    scrapping linkedin

--- dia3/1-scrapper_tipocambio.py
+++ dia3/1-scrapper_tipocambio.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.sunat.gob.pe/"
+url = "https://www.bcrp.gob.pe/"
 
 def obtener_tipocambio():
     response = requests.get(url)
     if(response.status_code == 200):
         html = BeautifulSoup(response.text,'html.parser')
-        ul_dolar = html.find('strong',{'id':'sell-rate'})
-        print(ul_dolar.get_text())
+        ul_dolar = html.find('table',{'class':'tip-table'})
+        print(ul_dolar)
     else:
         print(f"error : {response.status_code}")
 
