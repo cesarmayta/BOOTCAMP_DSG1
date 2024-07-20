82581cd3ae955ae79038735d8f2b718a3f1f6304
César Mayta <cesarmayta@gmail.com>
2024-07-19 22:05:08 -0500

    empresa ofertante

--- dia3/2-scrapper-linkedin.py
+++ dia3/2-scrapper-linkedin.py
@@ -14,16 +14,17 @@ def get_offers(skill):
 
         ul_offers = html.find('ul',{'class':'jobs-search__results-list'})
         li_offers = ul_offers.find_all('li')
-        #print(li_offers)
         
         for offer in li_offers:
             offer_title = offer.find('h3',{'class':'base-search-card__title'})
             offer_location = offer.find('span',{'class':'job-search-card__location'})
-            offer_company = offer.find('a')
-            offer_url = offer_company['href']
+            offer_url = offer.find('a')['href'].strip()
+            offer_company = offer.find('a',{'class':'hidden-nested-link'})
+            company = offer_company.get_text().strip() if offer_company else 'N/A'
+
             print(f'titulo : {offer_title.get_text().strip()}')
             print(f'ubicacion : {offer_location.get_text().strip()}')
-            print(f'empresa : {offer_company.get_text().strip()}')
+            print(f'empresa : {company}')
             print(f'url:{offer_url}')
     else:
         print(f"error : {url.status_code}")
