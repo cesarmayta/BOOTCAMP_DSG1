bbbe4748482c31ecda55cdcb68ae5cc24a89bc35
César Mayta <cesarmayta@gmail.com>
2024-07-24 20:05:42 -0500

    dia2

---
+++ dia2/01-prueba-apiperudev.py
@@ -0,0 +1,22 @@
+import requests
+
+token = ""
+url_dni = 'https://apiperu.dev/api/dni'
+
+dni = input("ingrese dni :")
+
+data_request = {
+    "dni": dni 
+}
+
+headers_request = {
+    "Authorization": "Bearer " + token,
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url_dni,json=data_request,headers=headers_request)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Error {response.status_code}")
